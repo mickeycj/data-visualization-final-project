427aa7b2301363d2d1fe2621368f30d59f7f3e8b
Chanon Jenakom <chanonjenakom@gmail.com>
2019-05-13 16:40:44 +0700

    Add apexcharts example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FinalProject';
+
+  title = {
+    test: 'Median Ratio'
+  };
+  chart = {
+    height: 350,
+    type: 'radialBar'
+  };
+  series = [67];
+  labels = ['Median Ratio'];
+  plotOptions = {
+    radialBar: {
+        startAngle: -135,
+        endAngle: 135,
+        dataLabels: {
+            name: {
+                fontSize: '16px',
+                color: undefined,
+                offsetY: 120
+            },
+            value: {
+                offsetY: 76,
+                fontSize: '22px',
+                color: undefined,
+                formatter: (val: any) => val + "%"
+            }
+        }
+    }
+  };
+  fill = {
+      type: 'gradient',
+      gradient: {
+          shade: 'dark',
+          shadeIntensity: 0.15,
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 50, 65, 91]
+      },
+  };
+  stroke = {
+      dashArray: 4
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgApexchartsModule } from 'ng-apexcharts';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
